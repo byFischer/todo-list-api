f2c550e8fe07ca4cf072e9be3cdadc4ad20043a6
unknown <byemrezeytin11@gmail.com>
2025-11-11 00:59:18 +0300

    Config Ayarı guncelleme

---
+++ src/main/java/com/example/todobackend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.todobackend.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://emrezeytin.org")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
