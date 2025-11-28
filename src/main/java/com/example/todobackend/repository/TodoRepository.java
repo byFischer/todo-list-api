6710620ce3e8d8d7f617f40e897656d314b563d8
Emre <115411304+byFischer@users.noreply.github.com>
2025-11-28 14:49:00 +0300

    Update TodoRepository.java

--- src/main/java/com/example/todobackend/repository/TodoRepository.java
+++ src/main/java/com/example/todobackend/repository/TodoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    // Ekstra method gerekmez, CRUD hazır
+   
 }
